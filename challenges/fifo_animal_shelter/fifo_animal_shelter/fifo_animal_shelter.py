0f57d5c86593f6254a844fdd76731e05475f96b4
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-06-21 21:14:39 +0300

    fifo

---
+++ challenges/fifo_animal_shelter/fifo_animal_shelter/fifo_animal_shelter.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    def enqueue(self, value):
+        node = Node(value)
+        if not self.rear:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+
+    def dequeue(self):
+        try:
+            temp = self.front
+            self.front = self.front.next
+            temp.next = None
+            return temp
+        except:
+            return 'this is an empty Stack'
+    def peek(self):
+        try:
+            return self.front.value
+        except:
+            return 'this is an empty Stack'
+    def is_empty(self):
+        return self.front == None
+
+
+
+
+
+# **************************************************************************************************
+class AnimalShelter(Queue):
+    
+    def __init__(self):
+        super().__init__()
+    def Animal_enqueue(self, animal):
+        if (animal == 'dog' or animal == 'cat'):
+            self.enqueue(animal)
+            return 'Successfully added the animal'
+        else:
+            return 'your input should only be a cat or a dog'
+            
+# this algorithm is built with a hint from google, not entirely by myself, which is the idea  of previous (i needed to create a 
+# previous by myself, but previous isn't available in Queues, so, the temp.next worked)
+    def Animal_dequeue(self , pref):
+        if not self.rear or not self.front:
+            return None
+        if (pref != 'dog' and pref != 'cat'):
+            return None
+        temp = self.front
+        if temp.value == pref:
+            a = self.dequeue().value
+            return a
+        else:
+            while temp.next != None:
+                if temp.next.value == pref:
+                    to_return = temp.next.value
+                    temp.next = temp.next.next
+                    return to_return
+                else:
+                    temp = temp.next
+            return None
+
+
+
+
+
+
+if __name__ == "__main__":
+    animals = AnimalShelter()
+    animals.Animal_enqueue('dog')
+    animals.Animal_enqueue('cat')
+    animals.Animal_enqueue('dog')
+    animals.Animal_enqueue('dog')
+    print(animals.Animal_dequeue("cat"))
+    print(animals.Animal_dequeue("dog"))
+    print(animals.Animal_dequeue("dog"))
+    print(animals.Animal_dequeue("dog"))
---
+++ challenges/fifo_animal_shelter/tests/test_fifo_animal_shelter.py
@@ -0,0 +1,39 @@
+from fifo_animal_shelter import __version__
+from fifo_animal_shelter.fifo_animal_shelter import *
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+# testing Animal_enqueue method against "cat" or "dog" inputs, also, testing against random inputs
+
+def test_Animal_enqueue():
+
+    animals = AnimalShelter()
+    assert animals.Animal_enqueue('cat') == 'Successfully added the animal'
+    assert animals.front.value == 'cat'
+    assert animals.rear.value == 'cat'
+    assert animals.Animal_enqueue('dog') == 'Successfully added the animal'
+    assert animals.front.value == 'cat'
+    assert animals.rear.value == 'dog'
+    assert animals.Animal_enqueue('humans') == 'your input should only be a cat or a dog'
+    assert animals.front.value == 'cat'
+    assert animals.rear.value == 'dog'
+
+# testing Animal_dequeue method against empty instance, values that aren't 'cat' or 'dog', or when there is no
+# more "dog" and "cat"
+
+def test_Animal_enqueue():
+    animals = AnimalShelter()
+    assert animals.Animal_dequeue('human') == None
+    assert animals.Animal_dequeue('cat') == None
+    animals.Animal_enqueue('cat')
+    animals.Animal_enqueue('dog')
+    animals.Animal_enqueue('cat')
+    animals.Animal_enqueue('dog')
+    assert animals.Animal_dequeue('cat') == 'cat'
+    assert animals.Animal_dequeue('cat') == 'cat'
+    assert animals.Animal_dequeue('cat') == None
+    assert animals.Animal_dequeue('dog') == 'dog'
+    assert animals.Animal_dequeue('dog') == 'dog'
+    assert animals.Animal_dequeue('dog') == None
